a6c3cd523c1d8bd2df66e7c13fecfd08554f9457
shubham <spcool555@gmail.com>
2025-09-10 20:56:53 +0530

    gfshjk

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230926163338/Java-Full-Stack.png" alt="Full Stack" className='w-full' />
 
       <br /><br />
-      <p style={{color:'red'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae deleniti voluptates non ipsam saepe atque repudiandae accusantium qui assumenda eum, inventore praesentium voluptatibus explicabo. Consequuntur.</p>
+      <p style={{color:'re -vd'}}>Lorem. Quae deleniti voluptates non ipsam saepe atque repudiandae accusantium qui assumenda eum, inventore praesentium voluptatibus explicabo. Consequuntur.</p>
       <PropsPractice abc="home" xyz="home.com" />
     </div>
   )
